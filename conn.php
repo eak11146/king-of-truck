7f912380bfbca53568169fe85cfc9991317b3103
eak11146 <evo008hp@gmail.com>
2024-12-03 09:50:09 +0700

    c

--- conn.php
+++ conn.php
@@ -2,17 +2,6 @@
 	  
 	include("conn/classdb_connect.php");
 	 
-	/*
-	$strHost = "localhost";
-	$strDB = "isuzu4u_new";
-	$strUser = "isuzu4u_new";
-	$strPassword = "dfTo56@3";
-	
-	
-	isuzu4u_new
-	isuzu4u_new
-	Nbg60h?7
-	*/
 	
 	$strHost = "localhost";
 	$strDB = "isuzu";
